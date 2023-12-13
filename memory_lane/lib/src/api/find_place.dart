5e6f3d7b34eeea6923853e0a8233463e65ec5cae
KeunHoJeon <68725481+ghtjd626@users.noreply.github.com>
2023-12-14 08:12:16 +0900

    fix, 에러핸들링 추가

--- memory_lane/lib/src/api/find_place.dart
+++ memory_lane/lib/src/api/find_place.dart
@@ -5,24 +5,42 @@ class LocationService {
   final String key = 'AIzaSyCne8NhkeffWaLxuydcuIONDrmUOwQpQ8w';
 
   Future<String> getPlaceId(String input) async {
-    late String url =
-        'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=$input&inputtype=textquery&key=$key';
-    final response = await http.get(Uri.parse(url));
-    final json = convert.jsonDecode(response.body);
-    final placeId = json['candidates'][0]['place_id'] as String;
+    try {
+      final url =
+          'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=$input&inputtype=textquery&key=$key';
+      final response = await http.get(Uri.parse(url));
 
-    return placeId;
+      if (response.statusCode == 200) {
+        final json = convert.jsonDecode(response.body);
+        final placeId = json['candidates'][0]['place_id'] as String;
+        return placeId;
+      } else {
+        throw Exception(
+            'placeID를 불러오는데 실패하였습니다. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('place ID 오류: $e');
+    }
   }
 
   Future<Map<String, dynamic>> getPlace(String input) async {
-    final placeId = await getPlaceId(input);
-    final String url =
-        'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&key=$key';
+    try {
+      final placeId = await getPlaceId(input);
+      final url =
+          'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&key=$key';
 
-    var response = await http.get(Uri.parse(url));
-    var json = convert.jsonDecode(response.body);
-    var results = json['result'] as Map<String, dynamic>;
+      var response = await http.get(Uri.parse(url));
 
-    return results;
+      if (response.statusCode == 200) {
+        var json = convert.jsonDecode(response.body);
+        var results = json['result'] as Map<String, dynamic>;
+        return results;
+      } else {
+        throw Exception(
+            '장소 상세정보를 불러오지 못하였습니다. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('place details 오류: $e');
+    }
   }
 }
